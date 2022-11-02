5307a3aa1209ef3a10dcfd674fde0050bf4a4021
PAVANIiii56 <96327277+PAVANIiii56@users.noreply.github.com>
2022-11-02 21:09:35 +0530

    def cou(n):     c=0     while n:         r=n%10         c+=1         n=n//10     return c n=int(input()) a=list(map(int,input().split())) c=99999 e=0 for i in range(n):   if cou(a[i])<c:         c=cou(a[i]) for i in range(n):     if cou(a[i])==c:         e+=1 print(e)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,18 @@
+def cou(n):
+    c=0
+    while n:
+        r=n%10
+        c+=1
+        n=n//10
+    return c
+n=int(input())
+a=list(map(int,input().split()))
+c=99999
+e=0
+for i in range(n):
+  if cou(a[i])<c:
+        c=cou(a[i])
+for i in range(n):
+    if cou(a[i])==c:
+        e+=1
+print(e)
